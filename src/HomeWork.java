dc4091b02336f945742616e40530eb7da372567a
Ceban Valentina <cebanvalentina9@gamil.com>
2026-05-26 15:16:29 +0300

    Add new class HomeWork

---
+++ src/HomeWork.java
@@ -0,0 +1,22 @@
+public class HomeWork {
+    public static void main(String[] args) {
+
+        System.out.println(numePrenume());
+        System.out.println(virsta());
+        System.out.println(inaltimea());
+
+    }
+
+    static String numePrenume() {
+        return "Ceban Valentina";
+    }
+
+    static int virsta() {
+        return 35;
+    }
+
+    static double inaltimea() {
+        return 1.67;
+    }
+}
+
